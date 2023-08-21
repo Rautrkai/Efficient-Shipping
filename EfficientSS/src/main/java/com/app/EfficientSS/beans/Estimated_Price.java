25a9ad55b71cca4a56e41dea55fd5cee05c74220
sid_17 <sid_17>
2023-08-21 23:37:36 +0530

    Initial commit

---
+++ EfficientSS/src/main/java/com/app/EfficientSS/beans/Estimated_Price.java
@@ -0,0 +1,11 @@
+package com.app.EfficientSS.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Estimated_Price {
+
+	@Id
+	private int id;
+}
---
+++ EfficientSS/src/main/java/com/app/EfficientSS/beans/Vehicle.java
@@ -0,0 +1,11 @@
+package com.app.EfficientSS.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicle {
+
+	@Id
+	private int id;
+}
